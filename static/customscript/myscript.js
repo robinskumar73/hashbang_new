e78076c729d518ed42666854002dcf9b4ca2e54c
Robins Gupta <robinskumar73@gmail.com>
2014-08-06 11:51:23 -0700

    Fixed NEXT AND PREV BUTTON

--- static/customscript/myscript.js
+++ static/customscript/myscript.js
@@ -1,3 +1,74 @@
 // JavaScript Document
 
 /*USER INTERFACE DESIGNS*/
+
+
+//Loading stories.....
+$(document).ready(function(e) {
+	var y = view.render().el;
+	y = $(y).children();
+   app.generate = app.GeneratePages( y );
+   init() ;
+
+});
+
+
+
+
+
+//------------------------------------------------ADDING THE KEYBOARD EVENTS---------------------------------------------------
+$(document).keydown(function(e) {
+    switch(e.which) {
+        case 37: // left
+		//Write keyboard events for LEft..
+		console.log("Turn to PREV Stories..");
+		
+        break;
+
+        case 39: // right
+		console.log("Turn to NEXT stories..");
+		turnNext()
+        break;
+
+        default: return; // exit this handler for other keys
+    }
+    
+});
+
+
+//Function for NEXT STORIES WITH ANIMATION EFFECT...
+var turnNext = function(){
+	//Function for turning to NEXT PAGE...
+	// Set the effect type
+    var effect = 'slide';
+ 
+    // Set the options for the effect type chosen
+    var options = { direction: 'left' };
+ 
+    // Set the duration (default: 400 milliseconds)
+    var duration = 1000;
+	$("#storyList").toggle(effect, options, duration);
+	init();
+	$("#storyList").toggle( "fade",{}, 0);
+	
+	
+}
+
+
+
+
+var init = function(){
+	//emptyColumns();
+	page = app.Pages;
+	app.generate.resetPage();
+	app.generate.createPage();
+	
+}
+
+
+var emptyColumns = function(){
+
+	$("#hashColumn1View").empty();
+	$("#hashColumn2View").empty();
+	$("#hashMainHalfStoryID").empty();
+}
--- static/js/app-main/hashbang-app-main.js
+++ static/js/app-main/hashbang-app-main.js
@@ -95,6 +95,14 @@ Modelstory = {
 }
 
 
+Modelstory1 = {
+	storyId:'1',
+	storyTitle: 'Lorem ipsum dolor sit amet, consectetur',
+	storyPosted: 'june-6th-2014',
+	storyContent: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis Lorem ipsum dolor sit amet, consectetur<br><br>Lorem ipsum dolor sit amet, mpor incididunt ut labore et dolore magna aliqua.<br><br> quis Lorem ipsum dolor sit amet, consectetu<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis Lorem ipsum dolor sit amet, consectetur<br><br>Lorem ipsum dolor sit amet, mpor incididunt ut labore et dolore magna aliqua. quis Lorem ipsum dolor sit amet, consectetu<br><br>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod' 
+}
+
+
 navLink = [ { navId:'1',navTitle:'Jecrc' },{ navId:'2',navTitle:'C.S.' },{ navId:'3',navTitle:'B-1 Batch',active:true } ]
 navLink1 = [ { navId:'1',navTitle:'Story2' },{ navId:'2',navTitle:'C.S.' },{ navId:'3',navTitle:'B-1 Batch',active:true } ]
 
@@ -107,18 +115,38 @@ hashTagMain = { tagId:'124', tagName:'#iLoveCricket', currentPromotions:'234520'
 postedHashTag = [{ tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'},{ tagId:'212', currentPromotions:'34351', tagName:'#LoveMyPhone'},{ tagId:'21242', currentPromotions:'351', tagName:'#Bitches'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'}, { tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'},{ tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'},{ tagId:'212', currentPromotions:'34351', tagName:'#LoveMyPhone'},{ tagId:'21242', currentPromotions:'351', tagName:'#Bitches'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'}, { tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'}, { tagId:'212', currentPromotions:'34351', tagName:'#DuniyaKaChutiyapa'},{ tagId:'21242', currentPromotions:'351', tagName:'#Bitches'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'},{ tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'},{ tagId:'212', currentPromotions:'34351', tagName:'#LoveMyPhone'},{ tagId:'21242', currentPromotions:'351', tagName:'#Bitches'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'}, { tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'} ]
 
 
+postedHashTag1 = [{ tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'},{ tagId:'212', currentPromotions:'34351', tagName:'#LoveMyPhone'},{ tagId:'21242', currentPromotions:'351', tagName:'#Bitches'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'},{ tagId:'212', currentPromotions:'34351', tagName:'#NoSleep'}, { tagId:'212', currentPromotions:'34351', tagName:'#TheGirlWhoDumpedMe'}]
+
+
 story = new app.Model.story(Modelstory, navLink, votesFlag, hashTagMain, postedHashTag );
 v = new app.Views.story({model:story});
 //el = $(v.render().el).children();
 //$("#hashColumn1View").append( el );
 
 Modelstory.storyId = '3';
-story_2  = new app.Model.story(Modelstory, navLink1, votesFlag, hashTagMain, postedHashTag );
-//story_3  = new app.Model.story(Modelstory, navLink, votesFlag, hashTagMain, postedHashTag );
+story_2  = new app.Model.story(Modelstory1, navLink1, votesFlag, hashTagMain, postedHashTag1);
+story_3  = new app.Model.story(Modelstory, navLink, votesFlag, hashTagMain, postedHashTag );
+story_4  = new app.Model.story(Modelstory1, navLink1, votesFlag, hashTagMain, postedHashTag1);
+story_5  = new app.Model.story(Modelstory, navLink, votesFlag, hashTagMain, postedHashTag );
 
 Stories.add(story);
 Stories.add(story_2);
-//Stories.add(story_3);
+Stories.add(story_3);
+Stories.add(story_4);
+Stories.add(story_5);
+
 
 //Adding stories views...
 view = new app.Views.storyList({collection: Stories});
+
+
+
+
+
+
+
+
+
+
+
+
